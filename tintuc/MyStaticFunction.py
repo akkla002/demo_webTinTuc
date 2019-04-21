e88603140aed6d938c2a56b03de5ef2cc8a27003
akkla002 <akkla002@gmail.com>
2019-04-21 12:56:58 +0700

    Change something in paginationOfLT

--- tintuc/MyStaticFunction.py
+++ tintuc/MyStaticFunction.py
@@ -1,16 +1,32 @@
+
+# Để có thể truy cập vào module models
+
 from tintuc.models import *
 
+# Đây là module chứa những static function
 
-numberOfRowToGet = 5
+
+theFolder='images/slide/'
+
+# Lấy dữ liệu để tạo menu Side bên Giao diện
+
 def loadMenuSide():
     return theloai.objects.all()
+
+
+# Lấy dữ liệu phần Slide đẩy lên Giao diện
+
 def loadTopSlide():
     arrSide=slide.objects.all()
-    folder='images/slide/'
     for sl in arrSide:
-        sl.Hinh=folder+sl.Hinh
+        sl.Hinh=theFolder+sl.Hinh
     return arrSide
+
+
+# Lấy những tin mới nhất theo thời gian
+
 def loadTopNews(page=None):
+    numberOfRowToGet = 5
     theFolder = 'images/tintuc/'
     fromPage = 0
     if page == None:
@@ -18,38 +34,34 @@ def loadTopNews(page=None):
     else:
         fromPage = numberOfRowToGet * (page-1)
     toPage = fromPage + numberOfRowToGet
-    topNews = Tin.objects.all().values('idTin','TieuDe','TieuDeKhongDau','TomTat','Hinh','SoLuotXem','Update_at','idLoaiTin')[fromPage:toPage]
+    topNews = Tin.objects.all().order_by('-Created_at').values('idTin','TieuDe','TieuDeKhongDau','TomTat','Hinh','SoLuotXem','Created_at','idLoaiTin')[fromPage:toPage]
     arrLT = []
     for eachNew in topNews:
         eachNew['Hinh'] = theFolder + eachNew['Hinh']
         temp = eachNew['idLoaiTin']
         arrLT.append(loaitin.objects.get(idLoaiTin=temp))
     return zip(topNews,arrLT)
-def loadNewOfTLWithPage(idTL,page = None):  
-    theFolder = 'images/tintuc/'
-    if page == None:
-        page = 1
-    else:
-        fromPage = numberOfRowToGet  * (page-1)
-    toPage = fromPage + numberOfRowToGet
-    arrNew = Tin.objects.filter(idLoaiTin=idTL).values('idTin','TieuDe','TieuDeKhongDau','TomTat','Hinh','SoLuotXem','Update_at')[fromPage:toPage]
-    for eachNew in arrNew:
-        eachNew['Hinh'] = theFolder + eachNew['Hinh']
-    return arrNew 
+
+
+
+# Lấy những dữ liệu cần thiết cho trang chi tiết (Lấy tin, comment, tin liên quan với tin tức đó)
+
 def loadInfoOfChiTiet(idTinTuc):
-    theFolder = 'images/tintuc/'
     tin = Tin.getTinWithFolderById(idTin=idTinTuc,folderString=theFolder)
     relatedNews = Tin.changeImageSource(tin.getRelatedNews(),theFolder)
     hotNews = Tin.changeImageSource(tin.getHotNews(), theFolder)
-    cms = tin.comment_set.all() #comment.objects.filter(idTinTuc=idTinTuc).values('idUser','NoiDung','Created_at')
-    if len(cms)==0:
+    cms = tin.comment_set.all().order_by('-Created_at') #comment.objects.filter(idTinTuc=idTinTuc).values('idUser','NoiDung','Created_at')
+    if len(cms) == 0:
         return {'TinTrang': tin,'relatedNews': relatedNews,'hotNews': hotNews}
     arrUser = []
     for cm in cms:
         arrUser.append(cm.getUserOfComment())
     return {'TinTrang': tin, 'AllComments': zip(cms,arrUser),'relatedNews': relatedNews,'hotNews': hotNews}
 
-def login(userName, passWord):
+
+# Kiểm tra thông tin khi đăng nhập
+
+def checkLogin(userName, passWord):
     user = users.login(userName,passWord)
     if user is None:
         return None
@@ -57,6 +69,13 @@ def login(userName, passWord):
     id = user.idUser
     theUser = {'name': name, 'idUser': id}
     return theUser
-def getMaxpageOfLoaiTin(idLoaiTin):
-    return loaitin.objects.all().count() / numberOfRowToGet + 1
 
+# thêm comment vào tin tức
+
+def addNewComment(idUser, idTin, commentContent):
+    cm = comment()
+    cm.idUser = users.objects.get(idUser=idUser)
+    cm.idTinTuc = Tin.objects.get(idTin=idTin)
+    cm.NoiDung = commentContent
+    cm.Created_at = datetime.datetime.now()
+    cm.save()
--- tintuc/views.py
+++ tintuc/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .MyStaticFunction import *
-from .pagination import *
+from .paginationOfLT import *
 
 # Create your views here.
-thePagination = pagination(0)
+thePagination = paginationOfLT(0)
 def LoadPage_index(request):
     if 'userName' not in request.session:
         request.session['userName'] = 'NULL'
@@ -12,26 +12,37 @@ def LoadPage_index(request):
     theSlide = loadTopSlide()
     Data = {'theloai': loadMenuSide(),'Slide': zip(theSlide,range(len(theSlide))),'Top5New':topFiveNews}
     return render(request,'pages/home.html',Data)
+
+
+
 def LoadPage_GioiThieu(request):
-    topFiveNews = Tin.objects.all()
+    topFiveNews = Tin.objects.all()[0:5]
     Data = {'theloai': loadMenuSide(),'TinTuc': topFiveNews}
     return render(request,'pages/GioiThieu.html',Data)
+
+
+# Lấy thông tin trang loaitin
 def LoadPage_loaitin(request, id, page = 1):
-    topFiveNews = loadNewOfTLWithPage(id,page=page)
-    if thePagination.theId != id:
-        thePagination.setId(id)
+    thePagination.setId(id)
     loaiTinTen = loaitin.objects.get(idLoaiTin=id)
     thePagination.nowPage = page
-    Data = {'theloai': loadMenuSide(),'Top5New': topFiveNews,'LoaiTinTen': loaiTinTen, 'idLoaiTin': id,
+    topNews = thePagination.getNewsInNowPage()
+    Data = {'theloai': loadMenuSide(),'TopNews': topNews,'LoaiTinTen': loaiTinTen, 'idLoaiTin': id,
     'nowPage':thePagination.nowPage,'maxPage': thePagination.maxPage,'theRangePages':thePagination.getRangePage()}
     return render(request,'pages/loaitin.html',Data)
 
 
 
-
+# Lấy view trang chitiet
 def LoadPage_chitiet(request, idTinTuc):
+    if request.method == "GET":
+        if 'commentContent' in request.GET and 'idUser' in request.session:
+            addNewComment(request.session['idUser'],idTinTuc, request.GET['commentContent'])
     Data = loadInfoOfChiTiet(idTinTuc)
     return render(request,'pages/chitiet.html',Data)
+
+
+
 def LoadPage_dangky(request):
     if request.method == "POST":
         return render(request,'pages/dangky.html')
@@ -39,18 +50,22 @@ def LoadPage_dangky(request):
 
 
 def LoadPage_dangnhap(request):
+    error = 'None'
+    # Kiểm tra thông tin đăng nhập có hay ko
     if request.method == "POST":
         if 'email' in request.POST:
             email = request.POST['email']
         if 'passWord' in request.POST:
-            passWord = request.POST['passWord']      
-        #request.session['userName'] = userName
-        theUser = login(email,passWord)
+            passWord = request.POST['passWord']     
+            
+        theUser = checkLogin(email,passWord)
         if theUser != None:
             request.session['userName'] = theUser['name']
             request.session['idUser'] = theUser['idUser']
-        return LoadPage_index(request)       
-    return render(request,'pages/dangnhap.html')
+            return LoadPage_index(request)
+        error = "Wrong email or password, please try again!"
+    Data = {'error': error}
+    return render(request,'pages/dangnhap.html',Data)
 def LoadPage_taikhoan(request):
     return render(request,'pages/taikhoan.html')
 
@@ -60,16 +75,3 @@ def LoadPage_logout(request):
     request.session.modified = True
     return LoadPage_index(request)
 
-
-
-def LoadPage_test(request, idTin):
-    a = Tin.objects.get(idTin=idTin)
-    cms = a.comment_set.all()
-    theUsers = []
-    if cms:
-        for cm in cms:
-            idUser = getattr(cm.idUser,'idUser')
-            name = users.objects.get(idUser = idUser)
-            theUsers.append(name)
-    dt = {'tin': a, 'comments': cms,'userName': theUsers}
-    return render(request,'pages/justWatchInfo.html', dt)
